278bd3b523f7bff5ac7bffca019bc670ed2c2051
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-07-17 13:48:01 -0700

    add correct createjs-easeljs package, imports loader ,exports loader, begin troubleshoot of config files

--- src/components/Control.js
+++ src/components/Control.js
@@ -1,13 +1,28 @@
+import * as createjs from 'createjs-easeljs';
 import React from 'react';
-import EaselJs from 'easeljs';
-
+import ReactDOM from 'react-dom';
 
 
 class Control extends React.Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    var canvas = ReactDOM.findDOMNode(this.refs.canvas);
+    this.stage = new createjs.Stage(canvas);
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 100;
+    circle.y = 100;
+    this.stage.addChild(circle);
+    this.stage.update();
+  }
+
   render() {
 
     return (
-      console.log("it works")
+      <canvas ref="canvas" width="500" height="600"></canvas>
     )
   }
 }
