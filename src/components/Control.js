719e09ac3daab7e8e900c1f4c06c74a430e8eff4
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-07-17 16:12:35 -0700

    successfully implement trail-marking with easeljs object

--- src/components/Control.js
+++ src/components/Control.js
@@ -18,17 +18,40 @@ class Control extends React.Component {
     circle.x = 100;
     circle.y = 100;
     this.stage.addChild(circle);
+    var square = new createjs.Shape();
+    square.graphics.beginFill("red").drawRect(5, 5, 5, 5)
+    square.x = 50;
+    square.y = 20;
+    this.stage.addChild(square);
+    let ticks;
     const handleTick = (event) => {
       circle.x += 3
+      square.x += 2
+      ticks = createjs.Ticker.getTicks()
       if (circle.x > this.stage.canvas.width) {
         circle.x = 0
+
+      }
+
+
+      if (ticks % 5 == 0) {
+        var dot = new createjs.Shape();
+        dot.graphics.beginFill("green").drawCircle(0, 0, 4)
+        dot.x = circle.x
+        dot.y = circle.y
+        this.stage.addChild(dot)
       }
       this.stage.update();
     }
+    const handleCircleClick = () => {
+
+    }
+    circle.on("click", handleCircleClick)
+
     createjs.Ticker.setFPS(30)
     createjs.Ticker.addEventListener("tick", handleTick)
 
-    this.stage.update()
+
   }
 
   render() {
