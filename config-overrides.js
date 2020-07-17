c62b711e0e0a8bfe8e34588a1c6638b204098408
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-07-17 14:04:19 -0700

    continue troubleshooting configuration, commit before ejecting from create-react-app

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+module.exports = function override(config, env) {
+  if (!config.plugins) {
+    config.plugins = [];
+  }
+
+  config.plugins.push(
+    (process.env.NODE_ENV === 'production') ?
+      new CopyWebpackPlugin([{ from: 'src/lib/legacyLib.js' }]) :
+      new CopyWebpackPlugin([{ from: 'src/lib/legacyLib.js', to: 'dist' }])
+  );
+
+  return config;
+}
