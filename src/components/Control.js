326aa396cc4cc42a4a97b2afd3631ce8c087c958
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-07-27 10:55:02 -0700

    refactor so grid variables are imported as variables rather than properties of object

--- src/components/Control.js
+++ src/components/Control.js
@@ -3,69 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Ant from './Ant';
 import Cell from './Cell';
-
+import * as grid from './Grid';
 
 class Control extends React.Component {
   constructor(props) {
     super(props);
-    this.coordArr = [];
-    this.tempCoords = [];
-    this.gridSize = 200;
-    this.maxAnts = 50;
-    this.antsOut = 0;
   }
 
-  createGrid = () => {
-    for (let x = 0; x < this.gridSize; x++) {
-      this.coordArr[x] = [];
-      this.tempCoords[x] = [];
-      for (let y = 0; y < this.gridSize; y++) {
-        this.coordArr[x][y] = new Cell(x, y);
-        this.tempCoords[x][y] = new Cell(x, y)
-      }
-    }
-  }
-  createRandomNum = (min, max) => {
-    return Math.floor(Math.random() * 200);
-  }
-  createRandomCoords = (x, y) => {
-    let a = this.createRandomNum(x + 1, x + 100);
-    let b = this.createRandomNum(y + 1, y + 100);
-    let z = this.createBound(a);
-    let q = this.createBound(b);
-    return [z, q]
-  }
-  createBound = (i) => {
-    let bound = i;
-    if (i < 0) {
-      bound = 0
-    }
-    if (i >= this.gridSize) {
-      bound = this.gridSize
-    }
-    return bound
-
-  }
   componentWillMount() {
-    this.createGrid()
-    for (let i = 0; i <= this.maxAnts; i++) {
-      this.spawnAnt()
+    grid.createGrid()
+    for (let i = 0; i <= grid.maxAnts; i++) {
+      grid.spawnAnt()
     }
+
     // console.log(this.coordArr)
-  }
-  spawnAnt = () => {
-    let x = 0;
-    let y = 0;
-    let newCoords = this.createRandomCoords(x, y);
-    var a = newCoords[0];
-    var b = newCoords[1];
-    if (!this.coordArr[a][b].hasAnt() && this.antsOut < this.maxAnts) {
-      this.coordArr[a][b].ant = new Ant();
-      this.tempCoords[a][b].ant = this.coordArr[a][b].ant
-      this.antsOut += 1;
-      console.log(this.coordArr[a][b].ant)
-    }
-    return [a, b]
   }
 
   componentDidMount() {
@@ -74,20 +25,21 @@ class Control extends React.Component {
     let canvas = ReactDOM.findDOMNode(this.refs.canvas);
     this.stage = new createjs.Stage(canvas);
     let ticks;
+    const makeAnt = (x, y) => {
+      let antDot = new createjs.Shape()
+      antDot.graphics.beginFill("Black").drawCircle(0, 0, 2)
+      antDot.x = x
+      antDot.y = y
+      this.stage.addChild(antDot)
+      this.stage.update()
+      console.log("MAKE ANT: " + x + "," + y)
+    }
     const handleTick = (event) => {
-      const makeAnt = (x, y) => {
-        let antDot = new createjs.Shape()
-        antDot.graphics.beginFill("Black").drawCircle(0, 0, 2)
-        antDot.x = x
-        antDot.y = y
-        this.stage.addChild(antDot)
-        this.stage.update()
-        console.log("MAKE ANT")
-      }
+
       ticks = createjs.Ticker.getTicks()
-      for (let x = 0; x <= this.gridSize; x++) {
-        for (let y = 0; y <= this.gridSize; y++) {
-          return console.log(this.coordArr[x][y].hasAnt() ? makeAnt(x, y) : null)
+      for (let x = 0; x <= grid.gridSize; x++) {
+        for (let y = 0; y <= grid.gridSize; y++) {
+          console.log(grid.coordArr[x][y])
         }
       }
 
@@ -104,7 +56,6 @@ class Control extends React.Component {
       //   }
       // }
 
-      this.stage.update();
     }
     // const handleCircleClick = () => {
 
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,3 +1,57 @@
-export default function Grid() {
+import Cell from './Cell';
+import Ant from './Ant';
 
+export let coordArr = [];
+export let tempCoords = [];
+export const gridSize = 200;
+export const maxAnts = 50;
+export let antsOut = 0;
+
+export const createGrid = () => {
+  for (let x = 0; x < gridSize; x++) {
+    coordArr[x] = [];
+    tempCoords[x] = [];
+    for (let y = 0; y < gridSize; y++) {
+      coordArr[x][y] = new Cell(x, y);
+      tempCoords[x][y] = new Cell(x, y)
+    }
+  }
+}
+
+export const createRandomNum = (min, max) => {
+  return Math.floor(Math.random() * 200);
+}
+
+export const createRandomCoords = (x, y) => {
+  let a = createRandomNum(x + 1, x + 100);
+  let b = createRandomNum(y + 1, y + 100);
+  let z = createBound(a);
+  let q = createBound(b);
+  return [z, q]
+}
+export const createBound = (i) => {
+  let bound = i;
+  if (i < 0) {
+    bound = 0
+  }
+  if (i >= gridSize) {
+    bound = gridSize
+  }
+  return bound
+
+}
+
+export const spawnAnt = () => {
+  let x = 0;
+  let y = 0;
+  let newCoords = createRandomCoords(x, y);
+  var a = newCoords[0];
+  var b = newCoords[1];
+  if (coordArr[a][b].hasAnt() == false && antsOut < maxAnts) {
+    coordArr[a][b].ant = new Ant();
+    tempCoords[a][b].ant = coordArr[a][b].ant
+    antsOut += 1;
+    console.log("ANTS:" + coordArr[a][b].ant)
+  }
+  return [a, b]
 }
